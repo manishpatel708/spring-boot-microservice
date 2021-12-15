cdba09a997bc575db80535c37238462b8904731a
manishpatel708 <52797545+manishpatel708@users.noreply.github.com>
2021-12-15 21:51:11 +0530

    Add files via upload
    hystrix dashboard added

---
+++ microservice/hystrix-dahsboard/src/test/java/com/microservice/hystrixdahsboard/HystrixDahsboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.hystrixdahsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDahsboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
